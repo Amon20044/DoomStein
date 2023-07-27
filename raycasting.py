c0cf89b058517d2a7575d65583779d97dd074a60
Amon Sharma <111745899+Amon20044@users.noreply.github.com>
2023-07-27 13:46:08 +0530

    raycasting.py

---
+++ raycasting.py
@@ -0,0 +1,13 @@
+import pygame as pg 
+import math
+from settings import *
+
+class RayCasting:
+    def __init__(self, game):
+        self.game = game
+        
+    def ray_cast(self):
+        pass
+    
+    def update(self):
+        self.ray_cast()
